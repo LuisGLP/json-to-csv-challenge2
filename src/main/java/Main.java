5209a47bda34ec4400a67be569e35980916ce098
Alberto García <galoop8@gmail.com>
2025-09-22 11:28:24 -0600

    Adding Documentation to Services and delimiter

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,41 @@ import services.ConverterService;
 
 import java.io.IOException;
 
+/**
+ * Entry point of the application.
+ * <p>
+ * This class initializes the conversion process from a JSON file to a CSV file.
+ * It accepts input and output file paths (and an optional delimiter) as command-line arguments.
+ * If no arguments are provided, it falls back to default paths.
+ * </p>
+ *
+ * <h2>Usage</h2>
+ * <pre>
+ *     java -jar json2csv.jar input.json output.csv ";"
+ * </pre>
+ *
+ * <p>Arguments:</p>
+ * <ul>
+ *   <li><b>args[0]</b> - Path to the input JSON file (required)</li>
+ *   <li><b>args[1]</b> - Path to the output CSV file (required)</li>
+ *   <li><b>args[2]</b> - Delimiter to use in the CSV file (optional, default = ",")</li>
+ * </ul>
+ *
+ * @author Luis
+ * @version 0.1
+ */
 public class Main {
     public static void main(String[] args) throws IOException {
         String inputPath;
         String outputPath;
+        String delimiter;
+
+        System.out.println("Usage: java -jar json2csv.jar <input.json> <output.csv>");
+
         if (args.length < 2) {
             inputPath = "src/main/resources/cities.json";
-            outputPath = "src/main/resources/output2.csv";
+            outputPath = "src/main/resources/output3.csv";
+            delimiter = ",";
 
             System.out.println("⚠ No arguments provided, using default paths:");
             System.out.println("Input JSON: " + inputPath);
@@ -16,10 +44,11 @@ public class Main {
         } else {
             inputPath = args[0];
             outputPath = args[1];
+            delimiter = args[2];
         }
 
         ConverterService converter = new ConverterService();
-        converter.convert(inputPath, outputPath);
+        converter.convert(inputPath, outputPath,delimiter);
         System.out.println("Converted JSON: " + outputPath);
         System.out.println("Converted CSV: " + outputPath);
     }
--- src/main/java/services/CsvWriterService.java
+++ src/main/java/services/CsvWriterService.java
@@ -6,30 +6,62 @@ import org.apache.commons.csv.CSVPrinter;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.RecordComponent;
+import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Service class responsible for writing data into CSV file.
+ * <p>
+ *     This class uses apache commons CSV to generate CSV file
+ *     with dynamic headers obtained from the model.
+ * </p>
+ *
+ * @author Luis
+ * @version 0.1
+ */
+
 public class CsvWriterService {
-    private static final  String[] HEADERS = {
-            "name", "capital",  "population", "area", "currency", "languages", "region", "subregion", "flag"
-    };
 
-    public void writerToCsv(List<CityRecord> cityRecords, String outputFile) throws IOException {
+    /**
+     * Writes a list of {@link CityRecord} into a CSV file.
+     *
+     * @param cityRecords List of cities records to be written.
+     * @param outputFile Path where the CSV file will be created.
+     * @param delimiter To separate the columns of the record.
+     * @throws IOException If there is an error while writing the file.
+     * @throws IllegalArgumentException If the record list is empty.
+     */
+
+    public void writerToCsv(List<CityRecord> cityRecords, String outputFile, String delimiter) throws IOException {
+        if (cityRecords.isEmpty()) {
+            throw new IllegalArgumentException("City records cannot be empty");
+        }
+
+        RecordComponent[] components = CityRecord.class.getRecordComponents();
+        String[] recordHeaders = Arrays.stream(components)
+                .map(RecordComponent::getName)
+                .toArray(String[]::new);
+
+        System.out.println("Validating headers...");
+        System.out.println("Record headers: " + String.join(delimiter, recordHeaders));
+
         try (CSVPrinter printer = new CSVPrinter(new FileWriter(outputFile),
-                CSVFormat.DEFAULT.withHeader(HEADERS))) {
-
-            for (CityRecord cityRecord : cityRecords) {
-
-                printer.printRecord(
-                        cityRecord.name(),
-                        cityRecord.capital(),
-                        cityRecord.population(),
-                        cityRecord.area(),
-                        cityRecord.currency(),
-                        cityRecord.languages(),
-                        cityRecord.region(),
-                        cityRecord.subregion(),
-                        cityRecord.flag()
-                );
+                CSVFormat.Builder
+                        .create()
+                        .setHeader(recordHeaders)
+                        .setDelimiter(delimiter)
+                        .build())) {
+            for (CityRecord record : cityRecords) {
+                Object[] values = new Object[components.length];
+                for (int i = 0; i < components.length; i++) {
+                    try {
+                        values[i] = components[i].getAccessor().invoke(record);
+                    } catch (Exception e) {
+                        values[i] = "";
+                    }
+                }
+                printer.printRecord(values);
             }
         }
     }
